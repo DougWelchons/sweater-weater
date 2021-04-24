fbbc4089e28964047af97da5cfaaad5a74d4ea3a
DougWelchons <doug.welchons@gmail.com>
2021-04-24 16:18:09 -0600

    Add test for services

---
+++ spec/services/forcast_service_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe WeatherService do
+  describe "class methods" do
+    describe ".get_weather" do
+      it "returns and OpenStruct object with current daily, and hourly weather" do
+        VCR.use_cassette("get_weather") do
+          cords = OpenStruct.new({lat: 48.38828, lng: -115.55581})
+          result = WeatherService.get_weather(cords)
+
+          expect(result).to be_a(OpenStruct)
+          expect(result.current_weather).to be_a(Hash)
+          expect(result.current_weather.keys).to eq([:datetime, :sunrise, :sunset, :temperature, :feels_like, :humidity, :uvi, :visibility, :conditions, :icon])
+          expect(result.daily_weather).to be_a(Array)
+          expect(result.daily_weather.first).to be_a(Hash)
+          expect(result.daily_weather.first.keys).to eq([:date, :sunrise, :sunset, :max_temp, :min_temp, :conditions, :icon])
+          expect(result.hourly_weather).to be_a(Array)
+          expect(result.hourly_weather.first).to be_a(Hash)
+          expect(result.hourly_weather.first.keys).to eq([:time, :temperature, :conditions, :icon])
+        end
+      end
+    end
+  end
+end
---
+++ spec/services/map_service_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe MapService do
+  describe "class methods" do
+    describe ".get_geocode" do
+      it "returns an OpenStruct object with the lat and lng as attrabutes" do
+        VCR.use_cassette("get_geocode") do
+          result = MapService.get_geocode("libby,mt")
+
+          expect(result).to be_a(OpenStruct)
+          expect(result.lat).to be_a(Float)
+          expect(result.lat).to eq(48.38828)
+          expect(result.lng).to be_a(Float)
+          expect(result.lng).to eq(-115.55581)
+        end
+      end
+    end
+  end
+end
