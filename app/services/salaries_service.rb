07d1335ad9b61fa30defe42a27fd5dfbcb21dfc9
DougWelchons <doug.welchons@gmail.com>
2021-04-26 10:26:50 -0600

    Add additional salaries service changes

--- app/services/salaries_service.rb
+++ app/services/salaries_service.rb
@@ -2,6 +2,24 @@ class SalariesService
 
   def self.get_salaries(destination)
     result = make_api_call("/api/urban_areas/slug%3A#{destination}/salaries")
+    jobs = ["Data Analyst",
+            "Data Scientist",
+            "Mobile Developer",
+            "QA Engineer",
+            "Software Engineer",
+            "Systems Administrator",
+            "Web Developer"]
+    salaries = result[:salaries].find_all do |job|
+      jobs.include?(job[:job][:title])
+    end
+    cleaned_salaries = salaries.map do |salary|
+      {
+        title: salary[:job][:title],
+        min: salary[:salary_percentiles][:percentile_25],
+        max: salary[:salary_percentiles][:percentile_75]
+      }
+    end
+    OpenStruct.new(salaries: cleaned_salaries)
     require "pry"; binding.pry
   end
 
