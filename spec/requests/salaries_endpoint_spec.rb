651c6c9292e86657153c5b6402a4b09766b63b29
DougWelchons <doug.welchons@gmail.com>
2021-04-26 09:29:59 -0600

    Add initial test structure

---
+++ spec/requests/salaries_endpoint_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe "app/v1/salaries endpoint" do
+  describe "Happy Path" do
+    it "returns a 200 response with relevant data" do
+      
+    end
+  end
+end
