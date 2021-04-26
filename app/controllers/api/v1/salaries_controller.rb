54dc18a9574f3993c175f46d5d4b17b9b90b2129
DougWelchons <doug.welchons@gmail.com>
2021-04-26 11:17:10 -0600

    Add passing feature test

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -1,11 +1,9 @@
 class Api::V1::SalariesController < ApplicationController
-  include ActionView::Helpers::NumberHelper
 
   def index
     cords = MapService.get_geocode(params[:destination])
     weather = WeatherService.get_current_weather(cords)
     salaries = SalariesService.get_salaries(params[:destination])
-    # require "pry"; binding.pry
     jobs = OpenStruct.new({id: nil, destination: params[:destination], forecast: {summary: weather.summary, temperature: weather.temperature}, salaries: salaries.salaries})
     render json: SalariesSerializer.new(jobs)
   end
--- spec/requests/salaries_endpoint_spec.rb
+++ spec/requests/salaries_endpoint_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe "app/v1/salaries endpoint" do
       get "/api/v1/salaries?destination=#{destination}"
       body = JSON.parse(response.body, symbolize_names: true)
 
-
       expect(response.status).to eq(200)
       expect(body).to be_a(Hash)
       expect(body.keys).to eq([:data])
@@ -18,10 +17,10 @@ RSpec.describe "app/v1/salaries endpoint" do
       expect(body[:data][:attributes]).to be_a(Hash)
       expect(body[:data][:attributes].keys).to eq([:destination, :forecast, :salaries])
       expect(body[:data][:attributes][:destination]).to eq(destination)
-      expect(body[:data][:attributes][:forcast]).to be_a(Hash)
-      expect(body[:data][:attributes][:forcast].keys).to eq([:summary, :temperature])
-      expect(body[:data][:attributes][:forcast][:summary]).to be_a(String)
-      expect(body[:data][:attributes][:forcast][:temperature]).to be_a(String)
+      expect(body[:data][:attributes][:forecast]).to be_a(Hash)
+      expect(body[:data][:attributes][:forecast].keys).to eq([:summary, :temperature])
+      expect(body[:data][:attributes][:forecast][:summary]).to be_a(String)
+      expect(body[:data][:attributes][:forecast][:temperature]).to be_a(String)
       expect(body[:data][:attributes][:salaries]).to be_a(Array)
       expect(body[:data][:attributes][:salaries].count).to eq(7)
       expect(body[:data][:attributes][:salaries].first).to be_a(Hash)
